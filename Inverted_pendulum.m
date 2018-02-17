8eee72b4c7f54ca97e7f3c6f22b373d2befd063c
anpoc <anpocard@gmail.com>
2018-02-17 11:48:55 -0500

    Add files via upload

--- Inverted_pendulum.m
+++ Inverted_pendulum.m
@@ -1,3 +1,7 @@
+%% Inicialization
+clear
+clc
+
 %% Non-linear system
 % Parameters
 M = 0.5; %(kg) mass of the cart
@@ -16,9 +20,33 @@ f2 = ((I+m*l^2)*u-(I+m*l^2)*b*x2+m^2*l^2*g*sin(y1)*cos(y1)-(I+m*l^2)*y2^2*sin(y1
 f3 = y2;
 f4 = (-m*l*cos(y1)*u+m*l*b*cos(y1)*x2+m^2*l^2*y2^2*sin(y1)*cos(y1)-(M+m)*m*g*l*sin(y1))/denominator;
 
-% Equilibrium points
-equilibrium = solve([f1,f2,f3,f4], [x1,x2,y1,y2,u]);
+% Output functions
+h1 = x1;
+h2 = y1;
 
+% Equilibrium points
+equilibrium = solve([f1, f2, f3, f4], [x1, x2, y1, y2, u]);
+
+%% Linealization for theta0=pi and other vars 0 to 0
+% Equilibrium values
+x10 = equilibrium.x1(4);
+x20 = equilibrium.x2(4);
+y10 = equilibrium.y1(4);
+y20 = equilibrium.y2(4);
+u0 = equilibrium.u(4);
+
+% Matrices
+A = jacobian([f1, f2, f3, f4], [x1, x2, y1, y2]);
+A = subs(A, [x1, x2, y1, y2], [x10, x20, y10, y20]);
+B = jacobian([f1, f2, f3, f4], u);
+B = subs(B, u, u0);
+C = jacobian([h1, h2], [x1, x2, y1, y2]);
+C = subs(C, [x1, x2, y1, y2], [x10, x20, y10, y20]);
+D = [0 ; 0];
+D = [0 ; 0];
+
+%% Equilibrium evaluation
+eigen = eig(A);
 %% Lineal (theta = y1 = pi+phi, phi aprox to 0)
 % System of equations
 % NOTE: given that phi is aprox 0, sin(pi+phi) and cos(pi+phi) are aprox
